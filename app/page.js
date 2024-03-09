91c1a697dc3644a9f7f104aabe81307d07cb3a20
Samil Abud <samilabud@gmail.com>
2024-03-09 11:27:19 -0400

    First commit, project created and .env added for future challenge revivew.

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
+  console.log(process.env.NASA_API_KEY);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
